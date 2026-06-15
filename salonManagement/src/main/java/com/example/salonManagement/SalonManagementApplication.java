05df63514dcf7be25704ff0652a916fe8ec3c72e
Umair <umairsanderwale318@gmail.com>
2026-06-15 20:43:08 +0530

    first commit and push

---
+++ salonManagement/src/main/java/com/example/salonManagement/SalonManagementApplication.java
@@ -0,0 +1,15 @@
+package com.example.salonManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalonManagementApplication {
+
+	public static void main(String[] args) {
+
+        SpringApplication.run(SalonManagementApplication.class, args);
+        System.out.println("Hello From Umair...");
+	}
+
+}
